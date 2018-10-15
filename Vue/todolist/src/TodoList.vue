d15d4086aa4c818ce5cc9bd987498e1ee637ff98
wzk <17761290450@163.com>
2018-10-15 17:57:13 +0800

    vue-cli

---
+++ Vue/todolist/src/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+  <div>
+    <label>
+      <input v-model="inputValue"/>
+    </label>
+    <button @click="handleSubmit">提交</button>
+  </div>
+  <ul>
+    <todo-item
+      v-for="(item,index) of list "
+      :key="index"
+      :content="item"
+      :index="index"
+      @delete="handleDelete"
+      ></todo-item>
+  </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  comments: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit: function () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/todolist/src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+<li @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['comtent', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
